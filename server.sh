7877ba2f41dc0bc7023a8e904ec000263a0d9fcb
Ellis Gibbons <ecgibbons@sep.com>
2026-09-25 13:29:34 -0400

    Prevent other trivial dos attacks

--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-ncat -lkC 8080 -c 'nvim --headless -u NONE -i NONE -s server.vim 2>/dev/null' --output server.log
+ncat -lkC 8080 -m 50 -c 'nvim --headless -u NONE -i NONE -s server.vim 2>/dev/null' --output server.log
